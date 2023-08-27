f545323f0c0c738ac8877e8b5f7f3bd205ac4ad3
Saruul <saruul@Saruuls-MacBook-Pro.local>
2023-08-28 02:18:34 +0800

    added index.tsx

--- website/src/components/Button/Button.tsx
+++ website/src/components/Button/Button.tsx
@@ -14,7 +14,6 @@ const Button: React.FC<ButtonProps> = ({
   text,
   type = 'button',
   variant = 'primary',
-  onClick,
   className
 }) => {
   // Define class names for different button variants using Tailwind CSS
@@ -26,7 +25,6 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       type={type}
-      onClick={onClick}
       className={`${variantClasses[variant]} px-4 py-2 rounded-md shadow-md transition duration-300 ease-in-out ${className}`}
     >
       {text}
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RootLayout from '@/app/layout';
+
+const HomePage = () => {
+  return (
+        <div>
+        <h1>Welcome to My Next.js Website</h1>
+        <p>This is a simple Next.js website example.</p>
+        </div>
+  );
+};
+
+export default HomePage;
